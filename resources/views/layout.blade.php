89827fd4757272f27a8e6276787c8153d814064e
Евгений <jonny153@mail.ru>
2018-04-27 13:23:45 +0200

    categories

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,6 +6,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
+        <meta name="yandex-verification" content="c72765daae3a7053" />
+
 
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
